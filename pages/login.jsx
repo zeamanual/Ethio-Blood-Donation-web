0f3cfdfc7f634883990e1251ea04d6bccaae93ff
abe <zeamanualfeleke@gmail.com>
2022-09-16 19:37:10 +0300

    log in page UI completed along with form validation for both sign up and log in pages

---
+++ pages/login.jsx
@@ -0,0 +1,130 @@
+import { AccountCircle, Visibility, VisibilityOff } from '@mui/icons-material'
+import { Alert, Box, Button, Grid, IconButton, InputAdornment, Link, Stack, TextField, Typography } from '@mui/material'
+import React from 'react'
+import Layout from '../components/layout'
+
+function Login() {
+    let [loginDetails,setLoginDetails]= React.useState({userName:'',password:''})
+    let [errorDetails,setErrorDetails]= React.useState({userName:{hasError:false,msg:''},password:{hasError:false,msg:''},isFormValid:false})
+    let [showPassword,setShowPassword]= React.useState(false)
+    let formSubmitHandler = (e)=>{
+        e.preventDefault();
+        if((!errorDetails.userName.hasError  && loginDetails.userName)  && (!errorDetails.password.hasError && loginDetails.password )){
+            console.log("no error")
+        }else{
+            console.log("form not submitted")
+        }
+        
+    }
+    let usernameChangeHandler = (e)=>{
+        setLoginDetails({...loginDetails,userName:e.target.value})
+        if(e.target.value.length<5){
+            setErrorDetails({
+                ...errorDetails,
+                userName:{
+                    hasError:true,
+                    msg:'User Name must be at least 5 characters'
+                },
+                isFormValid:false
+            })
+        }else{
+            setErrorDetails({
+                ...errorDetails,
+                userName:{
+                    hasError:false,
+                    msg:''
+                },
+                isFormValid:(true && !errorDetails.password.hasError && loginDetails.password)
+            })  
+        }
+    }
+    let passwordChangeHandler = (e)=>{
+        setLoginDetails({...loginDetails,password:e.target.value})
+        if(e.target.value.length<8){
+            setErrorDetails({
+                ...errorDetails,
+                password:{
+                    hasError:true,
+                    msg:'Password must be at least 8 characters'
+                },
+                isFormValid:false
+            })
+        }else{
+            setErrorDetails({
+                ...errorDetails,
+                password:{
+                    hasError:false,
+                    msg:''
+                },
+                isFormValid:(true && !errorDetails.userName.hasError && loginDetails.userName)
+            })  
+        }
+    }
+    let passwordvisibilityHandler = ()=>{
+        setShowPassword(!showPassword)
+    }
+  return (
+    <Layout>
+        <Box my={5} mx={{md:25,xs:2}} sx={{borderRadius:4,overflow:'hidden',boxShadow:'2px 2px 5px gray'}}>
+            <Grid container >
+                <Grid item xs={12} md={7}>
+                    <Box sx={{height:'100%',width:'100%',
+                  backgroundImage:`url(/login2.png)`,
+                  backgroundPosition:'center',
+                  backgroundSize:'contain',
+                  backgroundRepeat:'no-repeat',
+                  borderRadius:0,
+                  backgroundColor:'rgb(204, 204, 204)'
+                  }}> 
+
+                    </Box>
+                </Grid>
+
+                <Grid item xs={12} md={5}>
+                    <Box sx={{ackgroundColor:'yellow'}}> 
+                        <form onSubmit={formSubmitHandler}>
+                        <Stack px={6} py={15} gap={2}  direction={'column'}>
+                            <Box>
+                                <TextField fullWidth={true}  value={loginDetails.userName} onChange={usernameChangeHandler} size='small' label='Username' ></TextField>
+                            {errorDetails.userName.hasError ? <Alert sx={{padding:0,marginTop:1}} severity="error">{errorDetails.userName.msg}</Alert>:<></> }
+                            </Box>
+
+                            <Box>
+                            <TextField
+                            type={showPassword?'text':'password'}
+                            value = {loginDetails.password} 
+                            onChange={passwordChangeHandler} 
+                            size='small' label='password' 
+                            InputProps={{
+                            endAdornment: (
+                                <InputAdornment position='end'>
+                                    <IconButton onClick={passwordvisibilityHandler}>
+                                        {showPassword?<VisibilityOff />:<Visibility></Visibility>}
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                            }}
+                            fullWidth={true}
+                             ></TextField>
+                            {errorDetails.password.hasError ? <Alert sx={{padding:0,marginTop:1}} severity="error">{errorDetails.password.msg}</Alert>:<></> }
+                          
+                            </Box>
+                            <Box  display={'flex'} justifyContent={'center'}>
+                                <Button disabled={errorDetails.isFormValid?false:true} type='submit' variant='contained'>Log in</Button>
+                            </Box>
+                            <Box>
+                           <Typography align='center'>
+                           Don't have Habesha Donate account yet? <Link underline='hover' href="/signup" >Sign Up</Link>
+                           </Typography> 
+                            </Box>
+                        </Stack>
+                        </form>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    </Layout>
+  )
+}
+
+export default Login
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -1,4 +1,5 @@
-import { Box, Button, Grid, MenuItem, Stack, TextField, Typography } from '@mui/material'
+import { VerifiedUser, Visibility, VisibilityOff } from '@mui/icons-material'
+import { Box, Button, Grid, IconButton, InputAdornment, MenuItem, Stack, TextField, Typography } from '@mui/material'
 import React from 'react'
 import Layout from '../components/layout'
 
@@ -26,78 +27,150 @@ function SignUp() {
             longtitude:53.9
         }
     ]
-    let [fieldsValue,setFieldsValue]=React.useState({
-        userName:'',
-        phoneNumber:'',
-        email:'',
-        address:'',
-        bloodType:'',
-        password:'',
-        gender:'',
-        age:''
-    })
+
+    let [showPassword,setShowPassword] = React.useState(false)
+
+    let showPasswordHandler = ()=>{
+        setShowPassword(!showPassword)
+    }
 
     let handleNameChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            userName:e.target.value
-        })
-        // console.log(e.target.value,'user')
+        let value = e.target.value
+        if(value.length<5){
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                userName:{...fieldsValue.userName,hasError:true,msg:'username name must be at least 5 characters',value:e.target.value}
+            }))
+        }else{
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                userName:{...fieldsValue.userName,hasError:false,msg:'',value:e.target.value}
+            }))
+        }
     }
     let handlePhoneNumberChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            phoneNumber:e.target.value
-        })
-        // console.log(e.target.value,'phone')
+        if(e.target.value.length<10){
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                phoneNumber:{...fieldsValue.phoneNumber,hasError:true,msg:"Phone number too short",value:e.target.value}
+            }))
+        }else{
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                phoneNumber:{...fieldsValue.phoneNumber,hasError:false,msg:"",value:e.target.value}
+            }))
+        }
+     
     }
     let handleEmailChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            email:e.target.value
-        })
+        let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        let isValid = regex.test(e.target.value)
+        if(isValid){
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                email:{...fieldsValue.email,hasError:false,msg:'',value:e.target.value}
+            }))
+        }else{
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                email:{...fieldsValue.email,hasError:true,msg:'Enter a valid email',value:e.target.value}
+            })) 
+        }
         // console.log(e.target.value,'email')
     }
     let handleAddressChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            address:e.target.value
-        })
+        setFieldsValue(previousValue=>({
+            ...previousValue,
+            address:{...fieldsValue.address,hasError:false,msg:'',value:e.target.value}
+        }))
         // console.log(e.target.value,'address')
     }
     let handleAgeChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            age:e.target.value
-        })
+        let age = parseInt(e.target.value)
+        if(age<18){
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                age:{...fieldsValue.age,hasError:true,msg:'Age can not be under 18',value:e.target.value}
+            }))
+        }else{
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                age:{...fieldsValue.age,hasError:false,msg:'',value:e.target.value}
+            }))
+        }
         // console.log(e.target.value,'age')
     }
     let handleGenderChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            gender:e.target.value
-        })
+        setFieldsValue(previousValue=>({
+            ...previousValue,
+            gender:{...fieldsValue.gender,hasError:false,msg:'',value:e.target.value}
+        }))
         // console.log(e.target.value,'gender')
     }
     let handleBloodtypeChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            bloodType:e.target.value
-        })
+        setFieldsValue(previousValue=>({
+            ...previousValue,
+            bloodType:{...fieldsValue.bloodType,hasError:false,msg:'',value:e.target.value}
+        }))
         // console.log(e.target.value,'blood')
     }
     let handlePasswordChange=(e)=>{
-        setFieldsValue({
-            ...fieldsValue,
-            password:e.target.value
-        })
+        if(e.target.value.length<8){
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                password:{...fieldsValue.password,hasError:true,msg:'Password must be at least 8 characters',value:e.target.value}
+            }))
+        }else{
+            setFieldsValue(previousValue=>({
+                ...previousValue,
+                password:{...fieldsValue.password,hasError:false,msg:'',value:e.target.value}
+            }))
+        }
         // console.log(e.target.value,'pass')
     }
     let submitHandler = (e)=>{
         e.preventDefault();
-        console.log(fieldsValue)
+        let formValid = true
+        Object.keys(fieldsValue).forEach(fieldName=>{
+            if(fieldsValue[fieldName].value){
+                if(fieldsValue[fieldName].hasError){
+                    formValid=false
+                }
+            }else{
+
+                setFieldsValue((previousValue=>{
+                    return {
+                        ...previousValue,
+                        [fieldName]:{
+                            ...previousValue[fieldName],
+                            hasError:true,
+                            msg:`${fieldName} can not be empty`
+                        }
+                    }
+                }))
+                formValid=false
+            }
+        })
+        if(formValid){
+            console.log('form submitted',fieldsValue)
+        }else{
+            console.log('form not submitted',fieldsValue)
+
+        }
+        // console.log(fieldsValue)
         
     }
+    let [fieldsValue,setFieldsValue]=React.useState({
+        userName:{value:'',hasError:false,msg:'',changeHandler:handleNameChange},
+        phoneNumber:{value:'',hasError:false,msg:'',changeHandler:handlePhoneNumberChange},
+        email:{value:'',hasError:false,msg:'',changeHandler:handleEmailChange},
+        address:{value:'',hasError:false,msg:'',changeHandler:handleAddressChange},
+        gender:{value:'',hasError:false,msg:'',changeHandler:handleGenderChange},
+        age:{value:'',hasError:false,msg:'',changeHandler:handleAgeChange},
+        bloodType:{value:'',hasError:false,msg:'',changeHandler:handleBloodtypeChange},
+        password:{value:'',hasError:false,msg:'',changeHandler:handlePasswordChange},
+    })
+    // console.log("current status",fieldsValue)
   return (
     <Layout>
         <Box my={5} mx={{md:8,xs:2}} sx={{borderRadius:4,overflow:'hidden',boxShadow:'2px 2px 5px gray'}}>
@@ -118,20 +191,80 @@ function SignUp() {
 
                 <Grid xs={12} md={5}  item>
                 <form onSubmit={submitHandler}>
-
-                  <Stack padding={3} sx={{height:'100%',width:'100%'}}>
+                  
+                  <Stack padding={3} gap={1} sx={{height:'100%',width:'100%'}}>
                     <Typography align='center' my={2} variant='h5'>Enter Your Informaion Down Below</Typography>
-                    <TextField value ={fieldsValue.userName} onChange={handleNameChange} label='Username' variant='outlined' margin='dense' ></TextField>
-                    <TextField value ={fieldsValue.email} onChange={handleEmailChange} label='Email' variant='outlined' margin='dense' ></TextField>
-                    <TextField value ={fieldsValue.phoneNumber} onChange={handlePhoneNumberChange} label='Phone Number' variant='outlined' margin='dense' ></TextField>
-                    <TextField value ={fieldsValue.address} onChange={handleAddressChange} label='Address' variant='outlined' margin='dense' select >
+                  
+
+                  {Object.keys( fieldsValue).map(fieldName=>{
+                    let size = 'medium'
+                    let inputField = ''
+                    if(fieldName=='address'){
+                        inputField= (
+                            <TextField size={size}  error={fieldsValue[fieldName].hasError} helperText={fieldsValue[fieldName].msg} value ={fieldsValue[fieldName].value} onChange={fieldsValue[fieldName].changeHandler} label={fieldName} variant='outlined' select >
+                                {cities.map(city=>{
+                                    return  <MenuItem key={city.name} value={city.name} >{city.name}</MenuItem>
+                                })}
+                            </TextField>
+                        )
+                    }else if(fieldName =='gender'){
+                        inputField = (
+                            <TextField size={size} error={fieldsValue[fieldName].hasError} helperText={fieldsValue[fieldName].msg} value ={fieldsValue[fieldName].value} onChange={fieldsValue[fieldName].changeHandler} label={fieldName} select  variant='outlined'>
+                                <MenuItem value='Male'>
+                                    Male
+                                </MenuItem>
+                                <MenuItem value='Female'>
+                                    Female
+                                </MenuItem>
+                            </TextField>
+                          ) 
+                    }else if(fieldName=='bloodType'){
+                        inputField = (
+                            <TextField size={size} error={fieldsValue[fieldName].hasError} helperText={fieldsValue[fieldName].msg}  value ={fieldsValue[fieldName].value} onChange={fieldsValue[fieldName].changeHandler} label={fieldName} select variant='outlined' >
+                                {
+                                    bloodTypes.map(bloodType=>{
+                                        return <MenuItem key={bloodType} value={bloodType}>{bloodType}</MenuItem>
+                                    })
+                                }
+                            </TextField>
+                        )
+                    }else if(fieldName=='password'){
+                        inputField = (
+                            <TextField
+                            size={size}
+                            error={fieldsValue[fieldName].hasError} helperText={fieldsValue[fieldName].msg}
+                            type={showPassword ? 'text':'password'}
+                            value ={fieldsValue[fieldName].value} onChange={fieldsValue[fieldName].changeHandler} label={fieldName} variant='outlined'
+                            InputProps={{
+                                endAdornment:(
+                                    <InputAdornment position='end'>
+                                        <IconButton onClick={showPasswordHandler}>
+                                            {showPassword ? <VisibilityOff></VisibilityOff> :<Visibility></Visibility>}
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                            ></TextField>
+                        )
+                    }else{
+                        inputField=  (
+                            <TextField  size = {size} type={fieldName=='age' ?'number':'text'}   error={fieldsValue[fieldName].hasError} helperText={fieldsValue[fieldName].msg} value ={fieldsValue[fieldName].value} onChange={fieldsValue[fieldName].changeHandler} label={fieldName} variant='outlined' ></TextField>
+                        )
+                    }
+                    return inputField
+                  })}
+
+                    {/* <TextField   error={fieldsValue.userName.hasError} helperText={fieldsValue.userName.msg} value ={fieldsValue.userName.value} onChange={handleNameChange} label='Username' variant='outlined' margin='dense' ></TextField>
+                    <TextField error={fieldsValue.email.hasError} helperText={fieldsValue.email.msg} value ={fieldsValue.email.value} onChange={handleEmailChange} label='Email' variant='outlined' margin='dense' ></TextField>
+                    <TextField error={fieldsValue.phoneNumber.hasError} helperText={fieldsValue.phoneNumber.msg} value ={fieldsValue.phoneNumber.value} onChange={handlePhoneNumberChange} label='Phone Number' variant='outlined' margin='dense' ></TextField>
+                    <TextField error={fieldsValue.address.hasError} helperText={fieldsValue.address.msg}  value ={fieldsValue.address.value} onChange={handleAddressChange} label='Address' variant='outlined' margin='dense' select >
                         {cities.map(city=>{
                             return  <MenuItem key={city.name} value={city.name} >{city.name}</MenuItem>
                         })
                         }
                     </TextField>
-                    <TextField value ={fieldsValue.age} onChange={handleAgeChange} label='Age' variant='outlined' margin='dense' ></TextField>
-                    <TextField value ={fieldsValue.gender} onChange={handleGenderChange} label='Gender' select  variant='outlined' margin='dense' >
+                    <TextField error={fieldsValue.age.hasError} helperText={fieldsValue.age.msg} value ={fieldsValue.age.value} onChange={handleAgeChange} type={'number'} label='Age' variant='outlined' margin='dense' ></TextField>
+                    <TextField  error={fieldsValue.gender.hasError} helperText={fieldsValue.gender.msg} value ={fieldsValue.gender.value} onChange={handleGenderChange} label='Gender' select  variant='outlined' margin='dense' >
                         <MenuItem value='Male'>
                             Male
                         </MenuItem>
@@ -139,14 +272,28 @@ function SignUp() {
                             Female
                         </MenuItem>
                     </TextField>
-                    <TextField value ={fieldsValue.bloodType} onChange={handleBloodtypeChange} label='Blood Type' select variant='outlined' margin='dense' >
+                    <TextField  error={fieldsValue.bloodType.hasError} helperText={fieldsValue.bloodType.msg} value ={fieldsValue.bloodType.value} onChange={handleBloodtypeChange} label='Blood Type' select variant='outlined' margin='dense' >
                         {
                             bloodTypes.map(bloodType=>{
                                 return <MenuItem key={bloodType} value={bloodType}>{bloodType}</MenuItem>
                             })
                         }
                     </TextField>
-                    <TextField value ={fieldsValue.password} onChange={handlePasswordChange} label='password' variant='outlined' margin='dense' ></TextField>
+                    <TextField
+                    error={fieldsValue.password.hasError} 
+                    helperText={fieldsValue.password.msg}
+                    type={showPassword ? 'text':'password'}
+                    value ={fieldsValue.password.value} onChange={handlePasswordChange} label='password' variant='outlined' margin='dense'
+                    InputProps={{
+                        endAdornment:(
+                            <InputAdornment position='end'>
+                                <IconButton onClick={showPasswordHandler}>
+                                    {showPassword ? <VisibilityOff></VisibilityOff> :<Visibility></Visibility>}
+                                </IconButton>
+                            </InputAdornment>
+                        ),
+                    }}
+                    ></TextField> */}
                     <Box sx={{display:'flex',justifyContent:'center',marginTop:2}}>
                     <Button type='submit' variant='contained'>Sign Up</Button>
                     </Box>
