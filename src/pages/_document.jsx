33534f4524062e62e3a1b102514ae88b4bb05ad0
abe <zeamanualfeleke@gmail.com>
2022-11-22 09:49:06 +0300

    website logo changed

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,6 +9,13 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {this.props.emotionStyleTags}
+          <title>Habesha Donate</title>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"></link>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"></link>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"></link>
+          <link rel="manifest" href="/site.webmanifest"></link>
+          <meta name="msapplication-TileColor" content="#da532c"></meta>
+          <meta name="theme-color" content="#ffffff"></meta>
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
           <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet"></link>
